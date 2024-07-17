61bac20b8d24e9046c4be08fcab2399f59056ea4
Owen Powell <opowell@gmail.com>
2024-07-17 21:34:33 +0200

    bat cannot skip to door

--- model/MonsterFactory.js
+++ model/MonsterFactory.js
@@ -3,7 +3,7 @@ import { randomElement, randomInt } from "./utils.js"
 
 const randomMove = (monster, probRandom) => {
   if (Math.random() < probRandom) {
-    const locations = monster.game.getLocationsNearby(monster.location, location => location.canCharacterMoveTo)
+    const locations = monster.game.getLocationsNearby(monster.location, location => location.canCharacterMoveTo && monster.game.canCharacterMoveTo(monster.location, location))
     return randomElement(locations)
   } else {
     return monster.getMoveDestination(false)
